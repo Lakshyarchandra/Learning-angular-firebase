b684b21edf1057904d54968b1f96e0ff4dd7b51c
Lakshya Raj Chandra <lakshyarchandra@gmail.com>
2023-06-06 17:24:30 +0530

    Setup Done For Firebase

---
+++ Learning-angular-firebase/src/app/app.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { ApiService } from './api.service';
+import { AbstractControl, FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  public data: any;
+  public form = new FormGroup({
+    name: new FormControl('', [Validators.required]),
+  });
+  constructor(public apiService: ApiService) {
+    this.apiService.getAllData().then((data) => {
+      console.log(data);
+      this.data = data;
+    });
+  }
+
+  // public async onSubmit() {
+  //   console.log(this.form.value);
+  //   this.apiService.createData(this.form.value).then(() => {
+  //     window.location.reload();
+  //   });
+  // }
+
+  public get name(): AbstractControl {
+    return this.form.controls['name'];
+  }
+}
+  
+
---
+++ Learning-angular-firebase/src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    apiKey: "AIzaSyCQp4EUjoGRpwctJCM1fJK7wnV8hamuBLE",
+  authDomain: "learning-angular-firebas-63825.firebaseapp.com",
+  projectId: "learning-angular-firebas-63825",
+  storageBucket: "learning-angular-firebas-63825.appspot.com",
+  messagingSenderId: "1058993604198",
+  appId: "1:1058993604198:web:e52348f5a65eb4e7488fa7"
+  },
+};
